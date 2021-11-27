1b06e5a25f5030ba941cc75e44e59f279e578d18
LayKuan Loh <lkloh2410@gmail.com>
2021-11-27 10:19:36 -0800

    predict

--- logistic_regression/predict_university_admission/predict.py
+++ logistic_regression/predict_university_admission/predict.py
@@ -14,16 +14,55 @@ def load_data(filename):
     num_vars = len(contents)
 
     X = np.zeros(shape=(num_vars, 2))
-    Y = np.zeros(num_vars)
+    Y = np.zeros(num_vars, dtype=bool)
     for i, line in enumerate(contents):
         chunks = line.split(",")
         X[i][0] = float(chunks[0])
         X[i][1] = float(chunks[1])
-        Y[i] = float(chunks[2])
+        Y[i] = True if int(chunks[2]) == 1 else False
     return [X, Y]
+
+
+def visualize_data(X, Y):
+    X_admitted = X[Y]
+
+    Y_rejected = np.invert(Y)
+    X_rejected = X[Y_rejected]
+
+    fig = make_subplots(
+        rows=1,
+        cols=1,
+        x_title="Exam 1 Score",
+    )
+    fig.update_layout(title="Exam 2 Score against Exam 1 Score")
+
+    fig.append_trace(
+        go.Scatter(
+            x=X_admitted[:, 0],
+            y=X_admitted[:, 1],
+            name="Admitted",
+            mode="markers",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x=X_rejected[:, 0],
+            y=X_rejected[:, 1],
+            name="Rejected",
+            mode="markers",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.update_yaxes(title_text="Exam 2 Score", row=1, col=1)
+
+    fig.write_html("admitted_and_rejected_visualization.html")
 
 
 if __name__ == "__main__":
     [X, Y] = load_data("../assignment/ex2data1.txt")
-
-    print(Y)
+    visualize_data(X, Y)
