29aabead0756ec3fdf31e67c217e912369fa64e5
LayKuan Loh <lkloh2410@gmail.com>
2021-12-19 21:40:29 -0800

    neural net

--- neural_networks/multiclass_classification_with_neural_networks/classify.py
+++ neural_networks/multiclass_classification_with_neural_networks/classify.py
@@ -4,12 +4,18 @@ import numpy as np
 from scipy.io import loadmat
 
 HANDWRITTEN_DIGITS = loadmat("../assignment/ex3data1.mat")
+WEIGHTS = loadmat("../assignment/ex3weights.mat")
 
-NUM_LABELS = 10  # 10 labels, from 1, 2, ..., 9, 10
+NUM_LABELS = 10  # 10 labels, from 0, 2, ..., 9
+INPUT_LAYER_SIZE = 400  # 20x20 Input Images of Digits
+HIDDEN_LAYER_SIZE = 25
 
 if __name__ == "__main__":
     X = np.array(HANDWRITTEN_DIGITS["X"])
     Y = np.array(HANDWRITTEN_DIGITS["y"])
+    Y[Y == 10] = 0
 
+    theta1 = WEIGHTS["Theta1"]
+    theta2 = WEIGHTS["Theta2"]
 
-
+    print(theta1)
