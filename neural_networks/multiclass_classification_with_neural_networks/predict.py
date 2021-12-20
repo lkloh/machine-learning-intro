b585dc7a1e678603e832b219f66f0f3957a56241
LayKuan Loh <lkloh2410@gmail.com>
2021-12-19 22:13:15 -0800

    Update

--- neural_networks/multiclass_classification_with_neural_networks/predict.py
+++ neural_networks/multiclass_classification_with_neural_networks/predict.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 
+def add_intercept_term(arr):
+    (num_rows, _) = arr.shape
+
+    new_arr = np.c_[np.ones(shape=(num_rows, 1)), arr]
+    return new_arr
 
 def predict_classification(theta1, theta2, X):
     '''
@@ -12,6 +17,15 @@ def predict_classification(theta1, theta2, X):
     (num_labels, _) = theta2.shape
 
     p = np.zeros(shape=(num_samples, 1))
+
+    # Add intercept term to X
+    X = add_intercept_term(X)
+
+    hidden_layer = X @ theta1.transpose()
+    hidden_layer = add_intercept_term(hidden_layer)
+
+    output_layer = hidden_layer @ theta2.transpose()
+    
 
     return p
 
