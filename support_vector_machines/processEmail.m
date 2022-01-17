2af2b6ddbc0b8a63c0a8ee8c010ccd1eb7592e4b
LayKuan Loh <lkloh2410@gmail.com>
2022-01-17 11:59:14 -0800

    vocab list

--- support_vector_machines/processEmail.m
+++ support_vector_machines/processEmail.m
@@ -97,14 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    for i = 1:length(vocabList)
+        vocab_word = vocabList{i};
+        if strcmp(vocab_word, str) == 1
+            word_indices(end+1) = i;
+        end
+    end
 
     % =============================================================
 
